95e2e90102678835b840ff9bd687ad3b02be6fcb
Riccardo Colombo <rikyregna@gmail.com>
2020-07-20 15:47:12 +0200

    genero array numeri random senza duplicati

--- js/script.js
+++ js/script.js
@@ -11,5 +11,39 @@ con difficoltà 0 => tra 1 e 100
 con difficoltà 1 =>  tra 1 e 80
 con difficoltà 2 => tra 1 e 50 */
 
-// 1. Generare un array di 16 num casuali [1 - 100]
-// 2.
+// 1. Generare un array di N num casuali [1 - 100]
+// 2. Prompt per ricevere un numero dall'utente (100 - N volte)
+// 2.1 Controllo duplicati array utente
+// 3. Inserire il prompt in un ciclo:
+// se num utente non presente in num casuali pc ok altrimenti fine gioco
+// 4. comunicare il punteggio utente stabilito dalla lunghezza dell'array utente creato
+
+// 1. genero un array di 16 num casuali [1 - 100]
+
+var arrPc = [];
+
+while (arrPc.length < 16) {
+  var numeroPc = numRandom(1, 100);
+  if (!checkArr(arrPc, numeroPc)) {
+    arrPc.push(numeroPc);
+  }
+}
+
+console.log(`Array pc:${arrPc}`);
+
+/* funzioni */
+
+// funzione random
+function numRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// funzione check se un N è presente in un array (true = presente)
+function checkArr(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (num == arr[i]) {
+      return true;
+    }
+  }
+  return false;
+}
